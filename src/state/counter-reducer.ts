8c884ba7a3cda96978fbe4db93aeb95397461947
Shevchenko Andrey <demorest49de@gmail.com>
2024-04-02 21:23:13 +0300

    Changes counter-reducer.ts

--- src/state/counter-reducer.ts
+++ src/state/counter-reducer.ts
@@ -8,36 +8,14 @@ const SET_SET = 'SET_SET'
 const SET_ERROR = 'SET_ERROR'
 const SET_COUNTER = 'SET_COUNTER'
 
-export type IncAT =
-    ReturnType<typeof IncAC>
-
-export type ResetAT =
-    ReturnType<typeof ResetAC>
-
-export type SetMaxAT =
-    ReturnType<typeof SetMaxAC>
-
-export type SetStartAT =
-    ReturnType<typeof SetStartAC>
-
-export type SetSetAT =
-    ReturnType<typeof SetSetAC>
-
-export type SetErrorAT =
-    ReturnType<typeof SetErrorAC>
-
-export type SetCounterAT =
-    ReturnType<typeof SetCounterAC>
-
-type ActionType =
-    IncAT
-    | ResetAT
-    | SetMaxAT
-    | SetStartAT
-    | SetSetAT
-    | SetErrorAT
-    | SetCounterAT
-    ;
+export type IncAT = ReturnType<typeof IncAC>
+export type ResetAT = ReturnType<typeof ResetAC>
+export type SetMaxAT = ReturnType<typeof SetMaxAC>
+export type SetStartAT = ReturnType<typeof SetStartAC>
+export type SetSetAT = ReturnType<typeof SetSetAC>
+export type SetErrorAT = ReturnType<typeof SetErrorAC>
+export type SetCounterAT = ReturnType<typeof SetCounterAC>
+type ActionType = IncAT | ResetAT | SetMaxAT | SetStartAT | SetSetAT | SetErrorAT | SetCounterAT;
 
 
 const initialState = {
@@ -51,57 +29,26 @@ export type InitialStateType = typeof initialState
 
 export const counterReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
+        case INC: {
+
+        }
         case SET_MAX_VALUE: {
-            const nstate = {...state}
-            const hasError =
-                (+action.value < 0)
-                || (+nstate.start.inputValue < 0)
-                || (+action.value <= +nstate.start.inputValue)
-            nstate.hasErrorGlobal = hasError
-            nstate.max = {...nstate.max, inputValue: action.value, hasError}
-            nstate.start = {...nstate.start, hasError}
-            return nstate;
+
         }
         case SET_START_VALUE: {
-            const nstate = {...state}
-            const hasError =
-                (+action.value < 0)
-                || (+action.value >= +nstate.max.inputValue)
-            nstate.hasErrorGlobal = hasError
-            nstate.start = {...nstate.start, inputValue: action.value, hasError}
-            nstate.max = {...nstate.max, hasError}
-            return nstate;
-        }
-        case INC: {
-            const nstate = {...state}
 
-            const inc = (+nstate.start.inputValue + 1)
-            if (inc <= +nstate.max.inputValue) {
-                nstate.start.inputValue = inc.toString()
-            }
-            if (inc === +nstate.max.inputValue) {
-                nstate.incButtonDisabled = true
-            }
-
-            return nstate;
         }
         case SET_SET: {
-            const nstate = {...state}
-            nstate.setButtonDisabled = true
-            nstate.inputIsDisabled = true
-            nstate.incButtonDisabled = false
-            nstate.resetButtonDisabled = false
-            return nstate;
+
         }
         case RESET: {
-            const nstate = {...state}
-            nstate.setButtonDisabled = false
-            nstate.inputIsDisabled = false
-            nstate.max.inputValue = '2'
-            nstate.start.inputValue = '0'
-            nstate.incButtonDisabled = true
-            nstate.resetButtonDisabled = true
-            return nstate;
+
+        }
+        case SET_ERROR: {
+
+        }
+        case SET_COUNTER: {
+
         }
         default: {
             return state;
@@ -130,9 +77,9 @@ export const SetSetAC = () => {
 }
 
 export const SetErrorAC = () => {
-    return {type: RESET} as const
+    return {type: SET_ERROR} as const
 }
 
 export const SetCounterAC = () => {
-    return {type: RESET} as const
+    return {type: SET_COUNTER} as const
 }
