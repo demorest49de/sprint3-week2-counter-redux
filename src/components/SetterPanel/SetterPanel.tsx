02b276f3d5936cd775e3beda440e13a9552d7cf6
Shevchenko Andrey <demorest49de@gmail.com>
2024-04-03 23:48:22 +0300

    Changes Input.tsx SetterPanel.tsx

--- src/components/SetterPanel/SetterPanel.tsx
+++ src/components/SetterPanel/SetterPanel.tsx
@@ -10,27 +10,30 @@ import {AppStateType} from "../../state/store";
 import {SetErrorAC, SetMaxAC, StatusType} from "../../state/counter-reducer";
 
 
-
 export const SetterPanel = () => {
 
-    const maxValue = useSelector<AppStateType, number>(state => state.counter.maxValue)
-    const startValue = useSelector<AppStateType, number>(state => state.counter.startValue)
-    const status = useSelector<AppStateType, StatusType>(state => state.counter.status)
-    const dispatch = useDispatch()
-    console.log(' status: ', status);
-    console.log(' maxValue: ', maxValue);
+        const maxValue = useSelector<AppStateType, number>(state => state.counter.maxValue)
+        const startValue = useSelector<AppStateType, number>(state => state.counter.startValue)
+        const status = useSelector<AppStateType, StatusType>(state => state.counter.status)
+        const dispatch = useDispatch()
+        console.log(' status: ', status);
+        console.log(' maxValue: ', maxValue);
 
-    useEffect(()=>{
-        (maxValue <= startValue || startValue < 0 || maxValue <= 0)  && dispatch(SetErrorAC())
-    }, [status, maxValue, startValue, dispatch])
+        useEffect(() => {
+            (maxValue <= startValue || startValue < 0 || maxValue <= 0) && dispatch(SetErrorAC())
+        }, [status, maxValue, startValue, dispatch])
 
-    const changeMaxValue = (value: number)=>{
-        console.log(' value: ', value);
-        if(value > 99)  { return dispatch(SetMaxAC(99))}
-         if(value <= -1) { return  dispatch(SetMaxAC(1))}
+        const changeMaxValue = (value: number) => {
+            console.log(' value: ', value);
+            if (value > 99) {
+                return dispatch(SetMaxAC(99))
+            }
+            if (value <= -10) {
+                return dispatch(SetMaxAC(1))
+            }
 
-        dispatch(SetMaxAC(value))
-    }
+            dispatch(SetMaxAC(value))
+        }
 
 
         return (
@@ -47,25 +50,25 @@ export const SetterPanel = () => {
                             name={'max'}
                             focus={true}
                             inputValue={maxValue}
-                            hasError={maxValue <= startValue || maxValue <= 0 }
+                            hasError={maxValue <= startValue || maxValue <= 0}
                             callback={changeMaxValue}
                         />
-                    {/*    <Input*/}
-                    {/*        name={'start'}*/}
-                    {/*        inputValue={cp.start.inputValue}*/}
-                    {/*        hasError={cp.start.hasError}*/}
-                    {/*        onChangeStart={changeStartValue}*/}
-                    {/*        disabledState={cp.inputIsDisabled}*/}
-                    {/*    />*/}
-                    {/*</FieldSet>*/}
-                    {/*<FieldSet gradient={secondGradient}*/}
-                    {/*          buttonFieldSet={true}*/}
-                    {/*>*/}
-                    {/*    <Button*/}
-                    {/*        name={'set'}*/}
-                    {/*        setButtonHandler={setButtonHandler}*/}
-                    {/*        disabled={cp.hasErrorGlobal || cp.setButtonDisabled}*/}
-                    {/*    />*/}
+                        {/*    <Input*/}
+                        {/*        name={'start'}*/}
+                        {/*        inputValue={cp.start.inputValue}*/}
+                        {/*        hasError={cp.start.hasError}*/}
+                        {/*        onChangeStart={changeStartValue}*/}
+                        {/*        disabledState={cp.inputIsDisabled}*/}
+                        {/*    />*/}
+                        {/*</FieldSet>*/}
+                        {/*<FieldSet gradient={secondGradient}*/}
+                        {/*          buttonFieldSet={true}*/}
+                        {/*>*/}
+                        {/*    <Button*/}
+                        {/*        name={'set'}*/}
+                        {/*        setButtonHandler={setButtonHandler}*/}
+                        {/*        disabled={cp.hasErrorGlobal || cp.setButtonDisabled}*/}
+                        {/*    />*/}
                     </FieldSet>
                 </GradientWrapperStyled>
             </MainBlockStyled>
